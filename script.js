c404ea58ab4eff5966f2e82f50e0b23dd83a63b5
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-06-13 13:27:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const sidebar__container = document.getElementById('sidebar__container');
+
+function sidebar__opener() {
+  sidebar__container.style.transform = "scale(1)";
+}
+function sidebar__closer() {
+  sidebar__container.style.transform = "scale(0)";
+}
+
+const resultCon = document.getElementById('resultPage');
+
+
+//close resultPageClose 
+function resultPageClose(){
+  resultCon.style.transform = "scale(0)";
+}
+// Aman function 
+ function Aman() {
+  resultPage.style.transform = "scale(1)";
+  document.getElementById("result").src = "Result/Aman12.06.2021.png";
+ }
+// Shekhar function 
+ function Shekhar() {
+  resultPage.style.transform = "scale(1)";
+document.getElementById("result").src = "Result/Shekhar12.06.2021.png";
+ }
+// Nisha function 
+ function Nisha() {
+  resultPage.style.transform = "scale(1)";
+  document.getElementById("result").src = "Result/Nisha12.06.2021.png";
+ }
+// Supriya function 
+ function Supriya() {
+  resultPage.style.transform = "scale(1)";
+document.getElementById("result").src = "Result/Supriya12.06.2021.png";
+ }
+ // Zeba function 
+ function Zeba() {
+  resultPage.style.transform = "scale(1)";
+document.getElementById("result").src = "Result/Zeba12.06.2021.png";
+ }
+ 
