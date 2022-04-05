ac944902cc67336c06fa490a9b0367f6dee4e9ed
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-04-05 07:02:35 +0530

    features added

---
+++ studentData.js
@@ -0,0 +1,424 @@
+// This is the script where all the infomation of hcc studnet exist.
+const studentData = [
+    // student name
+    {
+      Name: "Shekhar Kumar ",
+      Joining: "07.06.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Shekhar.png",
+      section: "A",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/ShekharKumar.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Nisha Kumari",
+      Joining: "10.06.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Nisha.png",
+      section: "A",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/NishaKumari.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Supriya Kumari",
+      Joining: "07.06.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Supriya.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/SupriyaKumari.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Zeba Praveen",
+      Joining: "06.06.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Zeba.png",
+      section: "A",
+      Status: "Inactive",
+      result_page: "https://charmingprogrammer.github.io/HCC/ZebaPraveen.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Kajal Kumari",
+      Joining: "01.07.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Kajal.png",
+      section: "A",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Kajal.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Abhinash Kumar",
+      Joining: "29.06.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Abhinash.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Abhinash.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Jyotee Kumari",
+      Joining: "05.07.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Jyotee.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Jyotee.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Dhiraj Kumar",
+      Joining: "05.07.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Dhiraj.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Dhiraj.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Niraj Kumar",
+      Joining: "05.07.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Niraj.png",
+      section: "C",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Niraj.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Mistee Kumari",
+      Joining: "29.06.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Mistree.jpg",
+      section: "C",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Mistree.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Yash Raj",
+      Joining: "29.06.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Yash.png",
+      section: "C",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/YashRaj.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Manisha Kumari",
+      Joining: "23.03.2022",
+      Pic: "https://charmingprogrammer.github.io/HCC/Manisha.png",
+      section: "C",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/YashRaj.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Pritee Kumari",
+      Joining: "25.01.2022",
+      Pic: "https://charmingprogrammer.github.io/HCC/Pritee.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Pritee.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Niloo Kumari",
+      Joining: "26.12.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Niloo.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Niloo.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Nigam Kumar",
+      Joining: "24.12.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Nigam.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Nigam.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Nigam Kumar",
+      Joining: "24.12.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Nigam.png",
+      section: "C",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Nigam.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Krishna Kumar",
+      Joining: "29.12.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Krishna.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Krishna.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Arushi Kumari",
+      Joining: "03.11.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Arushi.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Arushi.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Aradhna Kumari",
+      Joining: "03.11.2021",
+      Pic: "https://charmingprogrammer.github.io/HCC/Aradhna.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Aradhna.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Gokul Kumar",
+      Joining: "02.04.2022",
+      Pic: "https://charmingprogrammer.github.io/HCC/Gokul.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Gokul.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+    // student name
+    {
+      Name: "Gautam Kumar",
+      Joining: "02.04.2022",
+      Pic: "https://charmingprogrammer.github.io/HCC/Gautam.png",
+      section: "B",
+      Status: "Active",
+      result_page: "https://charmingprogrammer.github.io/HCC/Gautam.html",
+      result_Info: [
+        {
+          Date: "12.09.2020",
+          Marks: "36%",
+          Progress: "Upward ↑",
+          Question_link: "https://manoarya.com/Question.html",
+        },
+        //   add in this week marks
+      ],
+    },
+    // end
+  ];
+  //  script  for pulling the main student data form studentData.js file
+  const students_data = document.getElementById("students_data");
+  // loop all the data
+  studentData.map((student) => {
+    console.log(student);
+    students_data.insertAdjacentHTML(
+      "beforeend",
+      `<a href="${student.result_page}">
+  <div class="box2">
+    <img src="${student.Pic}" alt="${student.Name}" />
+    <h3>${student.Name}</h3>
+    <p>
+    <b>
+      Joining Date : ${student.Joining} <br />
+      Section : ${student.section} |
+      Status : ${student.Status}</b>
+    </p>
+  </div>
+  </a> `
+    );
+  });
+  
