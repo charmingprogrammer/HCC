44975932504229be571370803130ad4122cdb53d
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2022-04-24 20:13:58 +0530

    update HCC_tracker_data.js

--- HCC_tracker_data.js
+++ HCC_tracker_data.js
@@ -2,18 +2,26 @@ console.log("HCC_tracker_data.js");
 
 const HCC_tracker_data = [
     {
-        date: "2019-01-01",
+        date: "18.04.2022",
         words : `
-        1. Apple 
-        2. Ball
-        3. Cat
-        4. Dog
-        5. Elephant`,
+        1. Proud : गर्व
+        2. Tree : पेड़
+        3. Forest : जंगल
+        4. Tall : लंबा
+        5. Next : अगला
+        6. Very : बहुत
+        7. Handsome : आकर्षक
+        8. Can : कर सकते हैं
+        9. Defeat : हराना
+        10. Me : मुझे`,
         spoken : `
-        1. Really good.
-        2. Very good.`,
+        1. Somehow : किसी न किसी तरह
+        2. Next time : अगली बार`,
         gk : `
-        1. Who is the first cheaf minister of bihar?`,
+        1. लाल किला कहाँ है ?
+        Ans : दिल्ली में  
+        2. ताजमहल कहाँ है ? 
+        Ans : आगरा में`,
         more : `
         1. loram ipsum`,
     },
